d5b0b5a5ed8273defa3b313775d6b421d0d006ee
benraydey <ben.j.adey@gmail.com>
2018-09-15 14:06:28 +0100

    Setup SPI interfacing

--- prac4.py
+++ prac4.py
@@ -1,31 +1,56 @@
 #Prac 4 python file
 #Commit 1
 
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import RPi.GPIO as GPIO
+import Adafruit_MCP3008
 import time
+import os
 from threading import Timer
 
+#Disable GPIO ste warnings
+GPIO.setwarnings(False)
+
 #setup GPIO pin selection method
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 
 #setup pins numbers for the different switches and outputs
-reset = 3
-frequency_pin = 5
-stop = 7
-display = 11
+reset = 2
+frequency_pin = 3
+stop = 4
+display = 17
+
+#setup pin numbers for SPI interface
+SPICLK = 11
+SPIMISO = 9
+SPIMOSI = 10
+SPICS = 8
 
 #setup variables for the program operation
 isDisplaying = 1                #is the progam displaying values, initialized to true
 frequency = 0.5                 #frequnecy of printing
-data[]={1,2,3}                  #dummy data varible (will need to implement a 2D array or something similar)
+data=[1,2,3]                  #dummy data variable (will need to implement a 2D array or something similar)
+
+values = [0]*8			# array for storing ADC values from MCP3008
+
 
 #setup GPIO input pins
 GPIO.setup(reset, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(frequency_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(stop, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(dsiplay, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(display, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+#setup GPIO io pins for SPI interface
+GPIO.setup(SPIMOSI, GPIO.OUT)
+GPIO.setup(SPIMISO, GPIO.IN)
+GPIO.setup(SPICLK, GPIO.OUT)
+GPIO.setup(SPICS, GPIO.OUT)
+
+#setup MCP3008
+mcp = Adafruit_MCP3008.MCP3008(clk=SPICLK, cs=SPICS, mosi=SPIMOSI, miso=SPIMISO)
+
+
 
 #function declarations
 def reset_callback(pin):
@@ -37,14 +62,14 @@ def frequency_pin_callback(pin):
 def stop_callback(pin):
 	print("Stop Callback")
 
-def stop_callback(pin):
+def display_callback(pin):
         print("Display Callback")
 
 def printing():
         global data
         for x in data:
                 print(x)
-        Timer(freqency,printing).start()
+        Timer(frequency,printing).start()
 
 #setup edge detection
 GPIO.add_event_detect(reset, GPIO.FALLING, bouncetime=200, callback=reset_callback)
@@ -55,15 +80,15 @@ GPIO.add_event_detect(display, GPIO.FALLING, bouncetime=200, callback=display_ca
 #setup GPIO output
 
 #start printing
-t = Timer(freqency,printing).start()
+t = Timer(frequency,printing).start()
 
 #put into program loop
 print("Setup done. Entering loop")
-try
+try:
         while True:
                 #Placeholder for later implementation
                 print("Data")
-                time.sleep(5)
+                time.sleep(1)
 except KeyboardInterrupt:
         GPIO.cleanup()  #cleanup GPIO on keyboard exit
 
